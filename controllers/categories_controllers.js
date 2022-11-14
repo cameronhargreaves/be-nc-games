44d491ab3419ff787f423a18a59b63769342dea4
Cameron Hargreaves <chargy2000@gmail.com>
2022-11-14 11:45:00 +0000

    finish task 3

--- controllers/categories_controllers.js
+++ controllers/categories_controllers.js
@@ -1,7 +1,9 @@
 const { selectCategories } = require("../models/categories_models");
 
-exports.getCategories = (req, res) => {
-  selectCategories().then((categories) => {
-    res.status(200).send({ categories });
-  });
+exports.getCategories = (req, res, next) => {
+  selectCategories()
+    .then((categories) => {
+      res.status(200).send({ categories });
+    })
+    .catch(next);
 };
