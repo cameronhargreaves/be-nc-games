3ad80f26b5b3d30fef4cef83915d9f00deed613a
Cameron Hargreaves <chargy2000@gmail.com>
2022-11-17 11:21:11 +0000

    routing issue

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,4 +1,4 @@
-const { getInfo } = require("./controllers/base_controller.js");
+const { getInfo } = require("../controllers/base_controller.js");
 const apiRouter = require("express").Router();
 // /api
 
